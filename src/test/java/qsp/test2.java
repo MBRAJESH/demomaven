69675e16e96cd76912321d733953c9b36f10a1a9
APM-SYSTEM <APM-SYSTEM@DESKTOP-488FP15>
2019-10-16 09:11:54 +0530

    new commit

---
+++ src/test/java/qsp/test2.java
@@ -0,0 +1,14 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test2 
+{
+	@Test
+	public void newdemo()
+	{
+		Reporter.log("new modification", true);
+	}
+
+}
